2a64f1eeed35a45d1567ce41e8b299f6719b440a
Dang Khoa <huynhtran.dangkhoa@gmail.com>
2018-06-06 00:43:35 +0700

    optimize server.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import './utils/logger'
 import dotenv from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser'
+import apicache from 'apicache'
+
+// For balancing.
+import cluster from 'cluster'
+import os from 'os'
+const cpus = os.cpus().length
 
 import mongoose from 'mongoose'
 
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import schema from './graphql'
+
+// For caching server.
+const cache = apicache.middleware
 
 dotenv.config()
 
@@ -20,7 +29,8 @@ const app = express()
 
 app.use([
   bodyParser.json(), 
-  bodyParser.urlencoded({ extended: false })
+  bodyParser.urlencoded({ extended: false }),
+  cache('60 minutes')
 ])
 
 app.use('/graphql', graphqlExpress((req, res) => {
@@ -34,6 +44,25 @@ app.use('/graphql', graphqlExpress((req, res) => {
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
-app.listen(process.env.PORT, () => {
-  console.info(`Server is running on port ${process.env.PORT}`)
+app.get('/', (req, res) => {
+   return res.status(200).send(`Food server.`)
 })
+
+if (cluster.isMaster) {
+  console.info(`Server is running on port: ${process.env.PORT}`)
+  console.info(`Master ${process.pid} is running`)
+
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.warn(`Worker ${worker.process.pid} died (${signal || code}). Restarting...`)
+
+    cluster.fork()
+  })
+} else {
+  app.listen(process.env.PORT, () => {
+    console.info(`Worker ${process.pid} started.`)
+  })
+}
